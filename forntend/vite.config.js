dbc2c5d57b92036289d0da8b46d92af86823427a
rohangoyal3012 <rohangoyal3012@gmail.com>
2025-12-06 02:02:05 +0530

    env fix,frontend typo fix

--- forntend/vite.config.js
+++ forntend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import dotenv from "dotenv";
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3002,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
